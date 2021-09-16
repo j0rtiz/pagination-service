04f53ccf1cc59a9a1a0e20d28629a61e37df7540
Jairo Ortiz <x369632@gruposantander.com>
2021-09-16 18:18:06 -0300

    feat(schema): Maximum page number

--- src/interfaces/http/schemas/pagination/paginationSchema.js
+++ src/interfaces/http/schemas/pagination/paginationSchema.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 module.exports = () =>
     Joi.object({
         query: Joi.object({
-            page: Joi.number().integer().positive().required(),
+            page: Joi.number().integer().positive().max(Joi.ref('pages')).required(),
             pages: Joi.number().integer().positive().required()
         }).required()
     }).required();
