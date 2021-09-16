267305b1a376e06ec29af9eb59c52c3bb19a3187
Jairo Ortiz <x369632@gruposantander.com>
2021-09-16 17:29:11 -0300

    feat(pagination): Factory

--- src/domain/factories/pagination/paginationFactory.js
+++ src/domain/factories/pagination/paginationFactory.js
@@ -1,6 +1,37 @@
 module.exports = () => ({
-    buildResult: (data) => {
-        const pagination = [data];
+    buildResult: ({ page, pages }) => {
+        page = Number(page);
+        pages = Number(pages);
+
+        let pagination = [];
+        const offset = 2;
+        const limit = 5;
+
+        for (let i = 1; i <= pages; i++) {
+            pagination.push(i);
+        }
+
+        const index = pagination.indexOf(page);
+        let start = index - offset < 0 ? 0 : index - offset;
+        let end = index + offset > pages ? pages : page + offset;
+
+        if (start === 0) {
+            end = start + limit;
+        }
+
+        if (pages - page < offset) {
+            start = pages - limit;
+        }
+
+        pagination = pagination.slice(start, end).map((pg) => (pg === page ? `**${pg}**` : String(pg)));
+
+        if (!['1', '**1**'].includes(pagination[0])) {
+            pagination.unshift('...');
+        }
+
+        if (![String(pages), `**${pages}**`].includes(pagination[pagination.length - 1])) {
+            pagination.push('...');
+        }
 
         return { pagination };
     }
